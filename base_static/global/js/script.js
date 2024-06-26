db630eae1b0670af744fe3db9d7425abb641fd10
GustavOxys <gustavohzmdev@gmail.com>
2024-06-26 10:02:23 -0300

    Navbar e menu-icon agora funcionam com script, acompanham a pagina e pode ser clicado em mobile para abrir navbar-mobile

--- base_static/global/js/script.js
+++ base_static/global/js/script.js
@@ -1,7 +1,7 @@
-let menuIcon = document.querySelector('menu-icon');
-let navbar = document.querySelector('navbar');
-let sections = document.querySelector('section');
-let navlinks = document.querySelector('header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -13,7 +13,7 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height){
             navlinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             })
         } 
     })
