3c75ab34620cc67c32efaf3be50983fb3e07211a
GustavOxys <gustavohzmdev@gmail.com>
2024-07-08 16:00:48 -0300

    adiciona tema claro e escuro

--- base_static/global/js/script.js
+++ base_static/global/js/script.js
@@ -23,3 +23,53 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggleLabel = document.getElementById('trocar-tema');
+  
+    // Conjuntos de cores
+    const originalColors = {
+      '--bg-color': '#ffffff',
+        '--bg-color-opacity': 'ff0000',
+        '--second-bg-color': '#f0f0f0',
+        '--text-color': '#000000',
+        '--main-color': '#54993e',
+        '--second-color': '#007bff'
+    };
+  
+    const newColors = {
+      '--bg-color': '#080808',
+      '--bg-color-opacity': 'rgba(0, 0, 0, 0.3)',
+      '--second-bg-color': '#131313',
+      '--text-color': 'white',
+      '--main-color': 'rgba(255, 251, 0, 0.89)',
+      '--second-color': '#00ffee'
+    };
+    
+  
+    let isOriginalTheme = true;
+  
+    // Função para alternar as variáveis CSS
+    function toggleTheme() {
+      const root = document.documentElement;
+      const colors = isOriginalTheme ? newColors : originalColors;
+  
+      // Altere as variáveis CSS
+      for (const [varName, varValue] of Object.entries(colors)) {
+        root.style.setProperty(varName, varValue);
+      }
+  
+      isOriginalTheme = !isOriginalTheme;
+    }
+  
+    // Adicionar o evento de clique à label
+    themeToggleLabel.addEventListener('click', toggleTheme);
+  });
+  
+  
