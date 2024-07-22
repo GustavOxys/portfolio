6a6283c8ff04028524956ce4f56360242d84c7bc
GustavOxys <gustavohzmdev@gmail.com>
2024-07-22 16:06:25 -0300

    Ao enviar a imagem do projeto agora ele redimensiona sozinho para o tamanho ideal

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils.text import slugify
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class Sobre(models.Model):
     primeiro_nome = models.CharField(max_length=10)
@@ -28,8 +31,28 @@ class Skill(models.Model):
 
 class Project(models.Model):
     name = models.CharField(max_length=25)
-    img = models.ImageField()
+    image = models.ImageField(upload_to='base_static/global/media/')
     text = models.TextField(max_length=470)
     skills = models.ManyToManyField(Skill, blank=True)
-    deploy = models.URLField(max_length=300)
-    repository = models.URLField(max_length=300)
+    deploy = models.URLField(max_length=300, blank=True)
+    repository = models.URLField(max_length=300, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.image and hasattr(self.image, 'file'):
+            # Redimensiona a imagem
+            image_file = self.image.file
+            image_pillow = Image.open(image_file)
+            new_image = self.resize_image(image_pillow, new_size=400)
+
+            # Salva a imagem redimensionada
+            image_io = BytesIO()
+            new_image.save(image_io, format='PNG', quality=60)
+            image_file = ContentFile(image_io.getvalue(), name=self.image.name)
+            
+            self.image.save(self.image.name, image_file, save=False)
+
+        super().save(*args, **kwargs)
+
+    def resize_image(self, image_pillow, new_size=400):
+        # Redimensiona a imagem para um quadrado
+        return image_pillow.resize((new_size, new_size), Image.LANCZOS)
--- utils/image.py
+++ utils/image.py
@@ -1,65 +1,21 @@
-from pathlib import Path
 from PIL import Image
+import io
+from django.core.files.base import ContentFile
 
-
-def resize_image_to_square(image_path, new_size=800, optimize=True, quality=60, output_path=None):
+def resize_image(image_file, new_size=400, optimize=True, quality=60):
+    # Abre a imagem
+    image = Image.open(image_file)
     
-    image_path = Path(image_path)
-    image_pillow = Image.open(image_path)
-
+    # Define o formato da imagem
+    format = image.format or 'PNG'  # Usa PNG como padrão se o formato não estiver definido
+    
     # Redimensiona a imagem para um quadrado
-    new_image = image_pillow.resize((new_size, new_size), Image.LANCZOS)
-
-    # Determina o caminho para salvar a nova imagem
-    if output_path is None:
-        output_path = image_path.parent / (image_path.stem + f"_resized{new_size}.png")
-    else:
-        output_path = Path(output_path)
-
-    # Salva a nova imagem
-    new_image.save(
-        output_path,
-        optimize=optimize,
-        quality=quality,
-    )
-
-    return new_image
-
-
-
-
-def resize_and_crop_image(image_path, new_size=(800, 800), optimize=True, quality=60, output_path=None):
+    image = image.resize((new_size, new_size), Image.LANCZOS)
     
-    image_path = Path(image_path)
-    image_pillow = Image.open(image_path)
-
+    # Salva a nova imagem em um buffer
+    image_io = io.BytesIO()
+    image.save(image_io, format=format, optimize=optimize, quality=quality)
     
-    image_pillow.thumbnail(new_size)
+    # Cria um ContentFile para o Django
+    return ContentFile(image_io.getvalue(), image_file.name)
 
-    
-    width, height = image_pillow.size
-    left = (width - new_size[0]) / 2
-    top = (height - new_size[1]) / 2
-    right = (width + new_size[0]) / 2
-    bottom = (height + new_size[1]) / 2
-
-    
-    cropped_image = image_pillow.crop((left, top, right, bottom))
-
-    
-    if output_path is None:
-        output_path = image_path.parent / (image_path.stem + "_cropped.jpg")
-    else:
-        output_path = Path(output_path)
-
-    
-    cropped_image.save(
-        output_path,
-        optimize=optimize,
-        quality=quality,
-    )
-
-    return cropped_image
-
-imagem = 'base_static/global/blog.png'
-resize_image_to_square(imagem, new_size=400)
